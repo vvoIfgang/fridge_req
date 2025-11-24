d74cad448d3d87045085a89a0f0505fd8c8b26aa
Your Name <you@example.com>
2025-11-24 20:06:34 +0900

    a

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
 const express = require("express");
 const app = express();
+require("dotenv").config();
 
 app.listen(8080, () => {
-  console.log("http://localhost:8080 에서 서버 실행중");
+  console.log(`http://localhost:8080 에서 서버 실행중`);
 });
 
 app.get("/", (req, res) => {
