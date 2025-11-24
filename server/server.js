a97e44b0f8095c9970ce126ef125a45c97bed3d7
Your Name <you@example.com>
2025-11-24 21:03:15 +0900

    userinfo테이블 create문

--- server/server.js
+++ server/server.js
@@ -1,9 +1,17 @@
 const express = require("express");
 const app = express();
+const maria = require("mariadb");
 require("dotenv").config();
 
-app.listen(8080, () => {
-  console.log(`http://localhost:8080 에서 서버 실행중`);
+const pool = maria.createPool({
+  host: process.env.Dbhost,
+  user: process.env.DbUser,
+  password: process.env.DbPw,
+  database: DataBase,
+});
+
+app.listen(process.env.PORT, () => {
+  console.log(`http://localhost:${process.env.PORT} 에서 서버 실행중`);
 });
 
 app.get("/", (req, res) => {
