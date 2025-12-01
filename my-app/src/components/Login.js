5119128096b90b7bf27bf2fb519d1d7cc363b1e5
Your Name <you@example.com>
2025-12-01 19:45:45 +0900

    변수수정

--- my-app/src/components/Login.js
+++ my-app/src/components/Login.js
@@ -21,7 +21,7 @@ function Login() {
     setError("");
 
     try {
-      const response = await fetch("http://localhost:8080/api/login", {
+      const response = await fetch("http://localhost:8080/api/auth/login", {
         method: "POST",
         headers: {
           "Content-Type": "application/json",
